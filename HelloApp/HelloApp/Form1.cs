7cad0fb080b3f2697406d2726c68f828d0cf7a49
Hoang NH <sq9bwfprvc@privaterelay.appleid.com>
2026-01-28 22:38:15 +0700

    Add hello button

--- HelloApp/HelloApp/Form1.cs
+++ HelloApp/HelloApp/Form1.cs
@@ -1,3 +1,6 @@
+using System.Windows.Forms;
+
+
 namespace HelloApp
 {
     public partial class Form1 : Form
@@ -5,6 +8,16 @@ namespace HelloApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("xin chào");
         }
     }
 }
